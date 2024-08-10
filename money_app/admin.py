9cbd22eee08fdbf58d4c300d773ecd527f3ca676
Anwar-Abdel <anwarabdeljaber0@gmail.com>
2024-08-10 18:26:19 -0500

    feat

--- money_app/admin.py
+++ money_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import Income, Expense, User
+
 
 admin.site.register(User)
+admin.site.register(Income)
+admin.site.register(Expense)
